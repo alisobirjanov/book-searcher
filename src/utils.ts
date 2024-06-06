9490e753d07e17eed267a9a90122e409a5e41f6e
Alimardon <alisobirzanov29@gmail.com>
2024-06-06 23:03:30 +0500

    feat: util functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,15 @@
 
 export const sleep = (timeout: number = 200) => new Promise(r => setTimeout(r, timeout))
+
+export function debounce<Params extends any[]>(
+  func: (...args: Params) => any,
+  timeout: number = 300,
+): (...args: Params) => void {
+  let timer: any
+  return (...args: Params) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      func(...args)
+    }, timeout)
+  }
+}
